2014915474b6b2b2e61df7219e208a6ce9536685
Dams4K <dams4k@users.noreply.github.com>
2025-07-22 21:17:16 +0200

    test snakeyaml

--- src/main/java/fr/dams4k/cpscore/CPSCore.java
+++ src/main/java/fr/dams4k/cpscore/CPSCore.java
@@ -1,8 +1,15 @@
 package fr.dams4k.cpscore;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
+
 public class CPSCore {
-    public String hello() {
-        return "world!";
+    public void test() {
+        Yaml yaml = new Yaml();
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("test.yaml");
+        Map<String, Object> data = yaml.load(is);
+        System.out.println(data);
     }
-    public int ihello() { return 1; }
 }
