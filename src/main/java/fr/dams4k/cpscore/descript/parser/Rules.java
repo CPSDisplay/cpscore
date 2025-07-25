24f1f9956f69c6c75e6ee4dd0cbf7bb55d7a5ed0
Dams4K <dams4k@users.noreply.github.com>
2025-07-25 15:33:34 +0200

    beebooop

---
+++ src/main/java/fr/dams4k/cpscore/descript/parser/Rules.java
@@ -0,0 +1,10 @@
+package fr.dams4k.cpscore.descript.parser;
+
+import fr.dams4k.cpscore.descript.lexer.TokenType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rules {
+    public Map<TokenType, TokenType[]> rules = new HashMap<>();
+}
